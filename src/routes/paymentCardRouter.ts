29acb77e2ad6f1e25f0ba043c3e690ad4c8e6077
Danil Kobzev <denthenttg@gmail.com>
2024-12-10 02:36:53 +0200

    fix getcards endpoint

--- src/routes/paymentCardRouter.ts
+++ src/routes/paymentCardRouter.ts
@@ -8,7 +8,7 @@ import {
 const router = express.Router();
 
 router.post('/createPaymentCard', createPaymentCard as express.RequestHandler);
-router.get('/getPaymentCardsByOwnerId', getPaymentCardsByOwnerId as express.RequestHandler);
+router.get('/getPaymentCardsByOwnerId/:ownerId', getPaymentCardsByOwnerId as express.RequestHandler);
 router.delete('/deletePaymentCard/:id', deletePaymentCard as express.RequestHandler);
 
 export default router;
