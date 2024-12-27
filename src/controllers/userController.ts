fac8e254786e9eed2be519247fe89f1d0a46f901
Alexandrus Mechanicus <sologub.sasha2001@gmail.com>
2024-12-27 23:10:37 +0200

    Add Service for Users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,34 +1,38 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User, { IUser } from '../models/User';
-import Device from '../models/Device';
-import PaymentCard from '../models/PaymentCard';
 import { AuthenticatedRequest } from '../interfaces/request.interface';
+import { UserService } from '../services/UserService';
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, surname, email, password } = req.body;
 
   try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
+    await UserService.createUser(name, surname, email, password);
+
+    res.status(201).json({ message: 'Користувача зареєстровано успішно.' });
+  } catch (error) {
+    if (error instanceof Error && error.message === 'BAD_REQUEST') {
       return res
         .status(400)
         .json({ message: 'Користувач із таким email вже існує.' });
     }
+    res.status(500).json({ message: 'Помилка сервера.', error });
+  }
+};
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+export const authenticateUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-    const newUser: IUser = new User({
-      name,
-      surname,
-      email,
-      password: hashedPassword,
-    });
-    await newUser.save();
+  try {
+    const token = await UserService.authenticateUser(email, password);
 
-    res.status(201).json({ message: 'Користувача зареєстровано успішно.' });
+    res.json({ token, message: 'Успішний вхід.' });
   } catch (error) {
+    if (error instanceof Error && error.message === 'NOT_FOUND') {
+      return res.status(404).json({ message: 'Користувача не знайдено.' });
+    }
+    if (error instanceof Error && error.message === 'BAD_REQUEST') {
+      return res.status(400).json({ message: 'Невірний пароль.' });
+    }
     res.status(500).json({ message: 'Помилка сервера.', error });
   }
 };
@@ -37,14 +41,13 @@ export const getUser = async (req: AuthenticatedRequest, res: Response) => {
   const id = req.user.id;
 
   try {
-    const user = await User.findById(id);
-
-    if (!user) {
-      return res.status(404).json({ message: 'Користувача не знайдено.' });
-    }
+    const user = await UserService.getUser(id);
 
     res.status(200).json(user);
   } catch (error) {
+    if (error instanceof Error && error.message === 'NOT_FOUND') {
+      return res.status(404).json({ message: 'Користувача не знайдено.' });
+    }
     res.status(500).json({ message: 'Помилка сервера.', error });
   }
 };
@@ -54,61 +57,15 @@ export const updateUser = async (req: AuthenticatedRequest, res: Response) => {
   const updates = req.body;
 
   try {
-    const updatedUser = await User.findByIdAndUpdate(id, updates, {
-      new: true,
-    });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'Користувача не знайдено.' });
-    }
+    const updatedUser = await UserService.updateUser(id, updates);
 
     res
       .status(200)
       .json({ message: 'Дані користувача оновлено.', updatedUser });
   } catch (error) {
-    res.status(500).json({ message: 'Помилка сервера.', error });
-  }
-};
-
-export const deleteUser = async (req: AuthenticatedRequest, res: Response) => {
-  const id = req.user.id;
-
-  try {
-    const deletedUser = await User.findByIdAndDelete(id);
-    await Device.deleteMany({ ownerId: id });
-    await PaymentCard.deleteMany({ ownerId: id });
-    if (!deletedUser) {
+    if (error instanceof Error && error.message === 'NOT_FOUND') {
       return res.status(404).json({ message: 'Користувача не знайдено.' });
     }
-
-    res.status(200).json({ message: 'Користувача видалено успішно.' });
-  } catch (error) {
-    res.status(500).json({ message: 'Помилка сервера.', error });
-  }
-};
-
-export const authenticateUser = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: 'Користувача не знайдено.' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Невірний пароль.' });
-    }
-
-    const token = jwt.sign(
-      { id: user._id, name: user.name, surname: user.surname },
-      process.env.JWT_SECRET as string,
-      { expiresIn: '2 days' },
-    );
-
-    res.json({ token, message: 'Успішний вхід.' });
-  } catch (error) {
     res.status(500).json({ message: 'Помилка сервера.', error });
   }
 };
@@ -121,20 +78,31 @@ export const changePassword = async (
   const { oldPassword, newPassword } = req.body;
 
   try {
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).json({ message: 'Користувача не знайдено.' });
-    }
+    await UserService.changePassword(id, oldPassword, newPassword);
 
-    const isMatch = await bcrypt.compare(oldPassword, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Старий пароль невірний.' });
-    }
-
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-    await User.findByIdAndUpdate(id, { password: hashedPassword });
     res.status(200).json({ message: 'Успішна зміна паролю.' });
   } catch (error) {
+    if (error instanceof Error && error.message === 'NOT_FOUND') {
+      return res.status(404).json({ message: 'Користувача не знайдено.' });
+    }
+    if (error instanceof Error && error.message === 'BAD_REQUEST') {
+      return res.status(400).json({ message: 'Старий пароль невірний.' });
+    }
+    res.status(500).json({ message: 'Помилка сервера.', error });
+  }
+};
+
+export const deleteUser = async (req: AuthenticatedRequest, res: Response) => {
+  const id = req.user.id;
+
+  try {
+    await UserService.deleteUser(id);
+
+    res.status(200).json({ message: 'Користувача видалено успішно.' });
+  } catch (error) {
+    if (error instanceof Error && error.message === 'NOT_FOUND') {
+      return res.status(404).json({ message: 'Користувача не знайдено.' });
+    }
     res.status(500).json({ message: 'Помилка сервера.', error });
   }
 };
