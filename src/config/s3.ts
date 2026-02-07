66eea4737ac48c906df4d115aeca8862c7ceca5a
Illya <katinskyi.illia@lll.kpi.ua>
2026-02-08 00:33:57 +0200

    Migrate to AWS SDK v3 and upgrade packages

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -1,29 +1,43 @@
-import AWS from 'aws-sdk';
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} from '@aws-sdk/client-s3';
 import { v4 as uuidv4 } from 'uuid';
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const BUCKET_NAME = 'eco-rent-images';
+
+const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
 });
 
-export const uploadToS3 = (file: Express.Multer.File) => {
+export const uploadToS3 = async (file: Express.Multer.File) => {
   const compressedFileName = file.originalname.trim().split(' ').join('');
-  const params: AWS.S3.Types.PutObjectRequest = {
-    Bucket: 'eco-rent-images',
-    Key: `${uuidv4()}-${compressedFileName}`,
-    Body: file.buffer,
-    ContentType: file.mimetype,
-  };
+  const key = `${uuidv4()}-${compressedFileName}`;
 
-  return s3.upload(params).promise();
+  await s3.send(
+    new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: key,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    }),
+  );
+
+  const location = `https://${BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+
+  return { Location: location };
 };
 
-export const deleteFromS3 = (imageUrl: string) => {
-  const params: AWS.S3.Types.DeleteObjectRequest = {
-    Bucket: 'eco-rent-images',
-    Key: imageUrl.split('/').pop() as string,
-  };
-
-  return s3.deleteObject(params).promise();
+export const deleteFromS3 = async (imageUrl: string) => {
+  await s3.send(
+    new DeleteObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: imageUrl.split('/').pop() as string,
+    }),
+  );
 };
