27fd022d198264632536de56a691c43c1ebafaef
Illya <katinskyi.illia@lll.kpi.ua>
2026-02-09 02:39:31 +0200

    Configure rate limit through env

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(helmet());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 1000,
+  max: parseInt(process.env.RATE_LIMIT_MAX || '1000', 10),
   standardHeaders: true,
   legacyHeaders: false,
   message: { message: 'Too many requests, please try again later.' },
